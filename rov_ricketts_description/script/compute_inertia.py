154ac3dcb8a9f42598eeb393fa7e21271e58b7c9
Alessandro Puglisi <puglisialessandro27@gmail.com>
2025-01-21 10:11:33 +0100

    debug inertia computation, remove normalization

--- rov_ricketts_description/script/compute_inertia.py
+++ rov_ricketts_description/script/compute_inertia.py
@@ -1,6 +1,7 @@
 # Python script to compute links inertia from material density and mesh file
 import trimesh
 import numpy as np
+from ament_index_python.packages import get_package_share_directory
 
 # Compute TITAN 4 Inertia: 
 density = 4.5  #[g/cm^3]
@@ -13,13 +14,14 @@ inertia_matrix = []
 
 i = 0
 for link in arm_links:
-    stl_file = "/home/ale/ros2_ws/src/rov_ricketts_description/meshes/" + link + ".stl"
+    mesh_path = get_package_share_directory('rov_ricketts_description')
+    stl_file = mesh_path + "/meshes/" + link + ".stl"
     mesh = trimesh.load(stl_file)
-    volume.append(mesh.volume) # [m^2]
+    volume.append(mesh.volume)              # [m^2]
     mass.append(mesh.volume*(density*1000)) # [Kg]
-    CoM.append(mesh.center_mass) #(1x3) [m]
-    # inertia_matrix.append(mesh.moment_inertia) #(3x3) [Kg*m^2/kg] Scaled Inertia
-    inertia_matrix.append(mesh.moment_inertia*mass[i]) # (3x3) [Kg*m^2] 
+    CoM.append(mesh.center_mass)            # [m] (1x3)
+    # inertia_matrix.append(mesh.moment_inertia) # [(Kg*m^2)/(kg*m^3)] (3x3) Normalized Inertia
+    inertia_matrix.append(mesh.moment_inertia*mass[i]/volume[i]) # (3x3) [Kg*m^2] 
 
     print(f"\nLink {link}: \n Volume= {volume[i]}\nMass= {mass[i]}\nCenter of Mass= {CoM[i]}\nInertia Matrix= {inertia_matrix[i]}\n------------------")
     i+=1
--- rov_ricketts_description/script/example_inertia.py
+++ rov_ricketts_description/script/example_inertia.py
@@ -9,15 +9,15 @@ density = 4500 #[kg/m^3]
 # Create a cylinder mesh
 cylinder = trimesh.creation.cylinder(radius=radius, height=height, sections=100) 
 mass = cylinder.volume * density
-
+V = cylinder.volume
 # Display cylinder properties
 print(f"Volume: {cylinder.volume} m^3")
 print(f"Center of Mass: {cylinder.center_mass}")
-inertia = cylinder.moment_inertia*mass
+inertia = cylinder.moment_inertia*mass/V
 print(f"Inertia Matrix:\n{inertia}")
 
 J = [[mass*(1/12*(height*height) + 1/4*(radius*radius)), 0, 0],[0, mass*(1/12*(height*height) + 1/4*(radius*radius)), 0],[0, 0, mass*1/2*(radius*radius)]]
 print(f"Analythic Inertia Matrix:\n{J}")
 
 # Visualize the cylinder
-cylinder.show()
+#cylinder.show()
