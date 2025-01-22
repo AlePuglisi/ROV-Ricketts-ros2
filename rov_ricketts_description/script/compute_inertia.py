61371f73cb205c1c32a729268b48659bb30bc68c
Alessandro Puglisi <puglisialessandro27@gmail.com>
2025-01-22 01:02:18 +0100

    add CoM centered meshes

--- rov_ricketts_description/script/compute_inertia.py
+++ rov_ricketts_description/script/compute_inertia.py
@@ -7,23 +7,28 @@ from ament_index_python.packages import get_package_share_directory
 density = 4.5  #[g/cm^3]
 
 arm_links = ["arm_base", "arm_link1", "arm_link2", "arm_link3", "arm_link4", "arm_link5", "grip_claw", "grip_claw"]
+#arm_CoM_links = ["arm_base_CoM", "arm_link1_CoM", "arm_link2_CoM", "arm_link3_CoM", "arm_link4_CoM", "arm_link5_CoM", "grip_claw_CoM", "grip_claw_CoM"]
 volume = []
 mass = []
 CoM = []
 inertia_matrix = []
+#inertia_matrix_CoM = []
 
 i = 0
 for link in arm_links:
     mesh_path = get_package_share_directory('rov_ricketts_description')
     stl_file = mesh_path + "/meshes/" + link + ".stl"
+    #stl_CoM_file = mesh_path + "/meshes/CoM_centered/" + link + "_CoM.stl"
     mesh = trimesh.load(stl_file)
+    #mesh_CoM = trimesh.load(stl_CoM_file)
     volume.append(mesh.volume)              # [m^2]
     mass.append(mesh.volume*(density*1000)) # [Kg]
     CoM.append(mesh.center_mass)            # [m] (1x3)
     # inertia_matrix.append(mesh.moment_inertia) # [(Kg*m^2)/(kg*m^3)] (3x3) Normalized Inertia
     inertia_matrix.append(mesh.moment_inertia*mass[i]/volume[i]) # (3x3) [Kg*m^2] 
+    #inertia_matrix_CoM.append(mesh_CoM.moment_inertia*mass[i]/volume[i])
 
-    print(f"\nLink {link}: \n Volume= {volume[i]}\nMass= {mass[i]}\nCenter of Mass= {CoM[i]}\nInertia Matrix= {inertia_matrix[i]}\n------------------")
+    print(f"\nLink {link}: \n Volume= {volume[i]}\nMass= {mass[i]}\nCenter of Mass= {CoM[i]}\nInertia Matrix Origin= {inertia_matrix[i]}\n------------------------")#Inertia Matrix CoM= {inertia_matrix_CoM[i]}\n------------------")
     i+=1
 
 print("\ntotal mass: " + str(sum(mass)))
