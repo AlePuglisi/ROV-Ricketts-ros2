9dd69fcd140895030b75da2a0ce7726218455565
Alessandro Puglisi <puglisialessandro27@gmail.com>
2025-02-09 20:19:45 +0100

    clean up code and add lights

--- rov_ricketts_description/script/compute_inertia.py
+++ rov_ricketts_description/script/compute_inertia.py
@@ -60,26 +60,28 @@ propeller_inertia_matrix = propeller_mesh.moment_inertia*propeller_mass/propelle
 
 print(f"\nPropeller Link: \n Volume= {propeller_volume}\nMass= {propeller_mass}\nCenter of Mass= {propeller_CoM}\nInertia Matrix Origin= {propeller_inertia_matrix}\n------------------------")
 
+# EXPERIMENTS ON SEPRATED BASE MESHES
+
 # Compute BASE-FLOAT separated Inertia
-base_mesh_name = "body_no_float"
-# base mass taken from MBARI's website
-base_mass = 4000
-mesh_path = get_package_share_directory('rov_ricketts_description')
-base_stl_file = mesh_path + "/meshes/stl/" + base_mesh_name + ".stl"
-base_mesh = trimesh.load(base_stl_file)
-base_volume = base_mesh.volume   
-base_CoM = base_mesh.center_mass          # [m] (1x3)
-base_inertia_matrix = base_mesh.moment_inertia*base_mass/base_volume # (3x3) [Kg*m^2] 
+# base_mesh_name = "body_no_float"
+# # base mass taken from MBARI's website
+# base_mass = 4000
+# mesh_path = get_package_share_directory('rov_ricketts_description')
+# base_stl_file = mesh_path + "/meshes/stl/" + base_mesh_name + ".stl"
+# base_mesh = trimesh.load(base_stl_file)
+# base_volume = base_mesh.volume   
+# base_CoM = base_mesh.center_mass          # [m] (1x3)
+# base_inertia_matrix = base_mesh.moment_inertia*base_mass/base_volume # (3x3) [Kg*m^2] 
 
-frame_mesh_name = "float"
-# base mass taken from MBARI's website
-frame_mass = 700
-frame_path = get_package_share_directory('rov_ricketts_description')
-frame_file = frame_path + "/meshes/" + frame_mesh_name + ".dae"
-frame_mesh = trimesh.load(frame_file)
-frame_volume = frame_mesh.volume   
-frame_CoM = frame_mesh.center_mass          # [m] (1x3)
-frame_inertia_matrix = frame_mesh.moment_inertia*frame_mass/frame_volume # (3x3) [Kg*m^2] 
+# frame_mesh_name = "float"
+# # base mass taken from MBARI's website
+# frame_mass = 700
+# frame_path = get_package_share_directory('rov_ricketts_description')
+# frame_file = frame_path + "/meshes/" + frame_mesh_name + ".dae"
+# frame_mesh = trimesh.load(frame_file)
+# frame_volume = frame_mesh.volume   
+# frame_CoM = frame_mesh.center_mass          # [m] (1x3)
+# frame_inertia_matrix = frame_mesh.moment_inertia*frame_mass/frame_volume # (3x3) [Kg*m^2] 
 
-print(f"\nBase Link: \n Volume= {base_volume}\nMass= {base_mass}\nCenter of Mass= {base_CoM}\nInertia Matrix Origin= {base_inertia_matrix}\n------------------------")
-print(f"\nFloat Link: \n Volume= {frame_volume}\nMass= {frame_mass}\nCenter of Mass= {frame_CoM}\nInertia Matrix Origin= {frame_inertia_matrix}\n------------------------")
+# print(f"\nBase Link: \n Volume= {base_volume}\nMass= {base_mass}\nCenter of Mass= {base_CoM}\nInertia Matrix Origin= {base_inertia_matrix}\n------------------------")
+# print(f"\nFloat Link: \n Volume= {frame_volume}\nMass= {frame_mass}\nCenter of Mass= {frame_CoM}\nInertia Matrix Origin= {frame_inertia_matrix}\n------------------------")
